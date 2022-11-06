c2306cf3c81015c4502de236e3760a2c520db221
Nathaniel Tampus <contact@nize.ph>
2022-11-06 20:53:01 +0800

    nvim treesitter config

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,45 @@
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all"
+  ensure_installed = {
+      "astro",
+      "c",
+      "cpp",
+      "html",
+      "css",
+      "scss",
+      "bash",
+      "go",
+      "java",
+      "javascript",
+      "lua",
+      "typescript",
+      "tsx",
+      "rust",
+      "sql",
+      "toml",
+      "yaml",
+      "json",
+      "json5"
+  },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = false,
+
+  highlight = {
+    -- `false` will disable the whole extension
+    enable = true,
+
+    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+    disable = function(lang, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+            return true
+        end
+    end,
+  },
+}
