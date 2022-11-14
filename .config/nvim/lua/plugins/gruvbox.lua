396debe5bf1ba01bbe8bfe9b4d958d79da242785
Nathaniel Tampus <contact@nize.ph>
2022-11-14 12:05:23 +0800

    improve sign column colors

--- .config/nvim/lua/plugins/gruvbox.lua
+++ .config/nvim/lua/plugins/gruvbox.lua
@@ -1,5 +1,15 @@
-local status_ok, _ = pcall(vim.cmd, "colorscheme gruvbox")
+local colors = require("gruvbox.palette")
+require("gruvbox").setup({
+  overrides = {
+    SignColumn = { bg = colors.dark0 },
+    GruvboxRedSign = { fg = colors.red, bg = colors.dark0, reverse = false },
+    GruvboxGreenSign = { fg = colors.green, bg = colors.dark0, reverse = false },
+    GruvboxYellowSign = { fg = colors.yellow, bg = colors.dark0, reverse = false },
+    GruvboxBlueSign = { fg = colors.blue, bg = colors.dark0, reverse = false },
+    GruvboxPurpleSign = { fg = colors.purple, bg = colors.dark0, reverse = false },
+    GruvboxAquaSign = { fg = colors.aqua, bg = colors.dark0, reverse = false },
+    GruvboxOrangeSign = { fg = colors.orange, bg = colors.dark0, reverse = false },
+  }
+})
 
-if not status_ok then
-    return
-end
+vim.cmd("colorscheme gruvbox")
